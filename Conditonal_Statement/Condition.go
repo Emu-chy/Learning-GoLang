6495be83879b758ff54b87bfdc00d3ac84335924
EMON KANTI SHIL <82772068+Emu-chy@users.noreply.github.com>
2025-12-21 00:45:09 +0600

    Simpale Conditional Statement

--- Conditonal_Statement/Condition.go
+++ Conditonal_Statement/Condition.go
@@ -3,5 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("this is test")
+	// conditional Statement (==, >, >=, <, <=, &&, ||)
+	age := 30
+	salary := 30000
+
+	if age == 20 {
+		fmt.Println("you are elegiable to merriage")
+
+	} else if age > 20 {
+		fmt.Println("you are elegiable to merriage and also ready to go out with your wife :)")
+	} else if age >= 20 {
+		fmt.Println("you are elegiable to merriage and also ready to take baby :)")
+	} else if age == 20 && salary == 30000 {
+		fmt.Println("you are elegiable to merriage and also ready to take two baby :)")
+	} else if age < 20 || salary == 30000 {
+		fmt.Println("you are not elegiable to merriage but if you want, you can love someone :)")
+	} else {
+		fmt.Println("you are not elegiable to merriage")
+	}
 }
---
+++ Conditonal_Statement/README.md
@@ -0,0 +1,22 @@
+
+# **Conditional Statements in Go** 
+
+Conditional statements help your program make decisions based on conditions. Think of them like a flowchart where your code can take different paths depending on certain situations.
+
+**Comparison Operators (Check conditions):** 
+
+**==** → "Is equal to?" (checks if two values are the same)
+
+**>** → "Is greater than?"
+
+**>=** → "Is greater than or equal to?"
+
+**<** → "Is less than?"
+
+**<=** → "Is less than or equal to?"
+
+ **Logical Operators (Combine conditions):**
+
+**&&** → "AND" → Both conditions must be true
+
+**||** → "OR" → At least one condition must be true
